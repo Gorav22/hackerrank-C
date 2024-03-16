9ae7988566953ff8ab2bcb0a8b35790babef4843
Gorav <goravjindal86@gmail.com>
2024-03-16 14:51:24 +0530

    day1 hackerrank commit 1st code

---
+++ d1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    printf("Hello, World!\n");
+    gets(s);
+    puts(s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
